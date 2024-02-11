3c0f0a70b841c458119507ea1188b31494560ca3
Daniel <72696191+danyaabros@users.noreply.github.com>
2024-02-11 11:27:57 +0100

    Update projects.js

--- projects.js
+++ projects.js
@@ -6,7 +6,7 @@
 
 // Массив с сайтами и параметрами
 const sites = [
-  { url: "https://abros.me", date: "01/01/2024", effect: "redirect(https://tractor-shop.ru)" },
+  { url: "https://abros.me", date: "01/01/2024", effect: "snow" },
   { url: "https://tractor-shop.ru", date: "23/01/2024", effect: "tanos" },
   { url: "https://asiana-food.ru", date: "01/04/2024", effect: "redirect(https://abros.me)" }
 ];
@@ -48,3 +48,4 @@ function checkDomain() {
 
 // Вызов функции для проверки домена
 checkDomain();
+
