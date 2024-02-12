3b49c7e11c9c391fcbf96b771eb607e01a410dfd
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-12 17:02:33 +0100

    Update q2.js

--- q2.js
+++ q2.js
@@ -6,7 +6,7 @@
 
 // Массив с сайтами и параметрами
 const sites = [
-  { url: "https://tractor-shop.ru", date: "23/01/2024", effect: "thanos" },
+  { url: "https://abros.me", date: "23/01/2024", effect: "thanos" },
   
 ];
 
