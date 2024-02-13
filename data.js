4582d34718da1a5f0c821fc6bb9922a79ec12a5b
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-13 16:36:42 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,3 @@
 var data = [
-  { name: "ABROS", link: "https://abros.me", date: "23/01/2024", effects: "none", redirecturl: "-", note: "Это сайт для тестов, с ним ничего не происходит." }
+    { name: "ABROS", url: "https://abros.me", date: "23/01/2024", effect: "none", redirecturl: "-", note: "Это сайт для тестов, с ним ничего не происходит." },
 ];
-
-function processData(data) {
-  var tableBody = $('#tableBody');
-
-  data.forEach(function (item) {
-    var row = '<tr>' +
-      '<td>' + item.name + '</td>' +
-      '<td>' + item.link + '</td>' +
-      '<td>' + item.date + '</td>' +
-      '<td>' + item.effects + '</td>' +
-      '<td>' + item.redirecturl + '</td>' +
-      '<td>' + item.note + '</td>' +
-      '</tr>';
-
-    tableBody.append(row);
-  });
-}
