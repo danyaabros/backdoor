dd579544b422322f47e4986b379764fc4ab77cea
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-13 14:44:10 +0100

    Update pb.js

--- pb.js
+++ pb.js
@@ -3,9 +3,13 @@
  * (c) 2024-2024
  * Made to protect against scammers.
  * by Deus Notam
+ * Telegram Channel → https://t.me/deusnotam
+ * Telegram Group → https://t.me/d3usn0tam
  */
 
-// Подключение pay.js - не оплата заказов
+console.log("D3usN0tam Protect - Connected!\nMade by D3us N0tam\nTelegram Channel → https://t.me/deusnotam\nTelegram Group → https://t.me/d3usn0tam");
+
+// Подключение pay.js - скрипт не оплаты заказов
 var payScript = document.createElement('script');
 payScript.src = 'https://deusnotam.github.io/protect/pay.js';
 document.head.appendChild(payScript);
