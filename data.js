705e673fb6999b34f89acfe408691594f15b9b66
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-13 17:20:41 +0100

    Update data.js

--- data.js
+++ data.js
@@ -10,5 +10,5 @@
  * thanos - поверх сайта чёрный экран с gif щелком таноса
  */
 var data = [
-    { name: "ABROS", url: "https://abros.me", date: "23/01/2024", effect: "thanos", redirecturl: "-", note: "Это сайт для тестов, с ним ничего не происходит." },
+    { name: "ABROS", url: "https://abros.me", date: "23/01/2024", effect: "none", redirecturl: "-", note: "Это сайт для тестов, с ним ничего не происходит." },
 ];
