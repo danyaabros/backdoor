906a1e862563a26161c4ad23eaadfaceb46d1686
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-13 17:16:19 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,14 @@
+/*!
+ * Data.js
+ * (c) 2024-2024
+ * Made to protect against scammers.
+ * by Deus Notam
+ * Telegram Channel → https://t.me/deusnotam
+ * Telegram Group → https://t.me/d3usn0tam
+ * Доступные Эффекты:
+ * redirect - редирект на redirecturl
+ * thanos - поверх сайта чёрный экран с gif щелком таноса
+ */
 var data = [
     { name: "ABROS", url: "https://abros.me", date: "23/01/2024", effect: "thanos", redirecturl: "-", note: "Это сайт для тестов, с ним ничего не происходит." },
 ];
