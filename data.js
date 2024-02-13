0a3f382e169c0f279b86e995a4422ac452e4cd95
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-13 17:12:58 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
 var data = [
-    { name: "ABROS", url: "https://abros.me", date: "23/01/2024", effect: "none", redirecturl: "-", note: "Это сайт для тестов, с ним ничего не происходит." },
+    { name: "ABROS", url: "https://abros.me", date: "23/01/2024", effect: "thanos", redirecturl: "-", note: "Это сайт для тестов, с ним ничего не происходит." },
 ];
