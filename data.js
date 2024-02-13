76593ea031769fda6a541ab6b2d94f14db159b7d
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-13 16:22:51 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,20 @@
-processData([
-  { url: "https://abros.me", date: "23/01/2024", effect: "none" },
-  
-]);
+var data = [
+  { name: "ABROS", link: "https://abros.me", date: "23/01/2024", effects: "none", redirecturl: "-", note: "Это сайт для тестов, с ним ничего не происходит." }
+];
+
+function processData(data) {
+  var tableData = data.map(function (item) {
+    return {
+      name: item.name,
+      link: item.link,
+      date: item.date,
+      effects: item.effects,
+      redirecturl: item.redirecturl,
+      note: item.note
+    };
+  });
+
+  $('#table').bootstrapTable('load', tableData);
+}
+
+processData(data);
