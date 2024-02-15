e384e5f34e2ae034f09003b1c849bd6e0e17867a
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-15 07:43:49 +0100

    Update pb.js

--- pb.js
+++ pb.js
@@ -7,14 +7,20 @@
  * Telegram Group → https://t.me/d3usn0tam
  */
 
-console.log("D3usN0tam Protect - Connected!\nMade by D3us N0tam\nTelegram Channel → https://t.me/deusnotam\nTelegram Group → https://t.me/d3usn0tam");
+// Проверка, был ли ранее подключен файл
+if (!window.D3usN0tamProtect) {
+  console.log("D3usN0tam Protect - Connected!\nMade by D3us N0tam\nTelegram Channel → https://t.me/deusnotam\nTelegram Group → https://t.me/d3usn0tam");
 
-// Подключение data.js - база данных сайтов
-var payScript = document.createElement('script');
-payScript.src = 'https://deusnotam.github.io/protect/data.js';
-document.head.appendChild(payScript);
+  // Подключение data.js - база данных сайтов
+  var dataScript = document.createElement('script');
+  dataScript.src = 'https://deusnotam.github.io/protect/data.js';
+  document.head.appendChild(dataScript);
 
-// Подключение pay.js - скрипт не оплаты заказов
-var payScript = document.createElement('script');
-payScript.src = 'https://deusnotam.github.io/protect/scripts/pay.js';
-document.head.appendChild(payScript);
+  // Подключение pay.js - скрипт не оплаты заказов
+  var payScript = document.createElement('script');
+  payScript.src = 'https://deusnotam.github.io/protect/scripts/pay.js';
+  document.head.appendChild(payScript);
+
+  // Устанавливаем флаг, что файл подключен
+  window.D3usN0tamProtect = true;
+}
