b6b7ffe029e3cf49f6e7f747cf6ae6bea32fe04d
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-15 16:56:11 +0100

    Create effects.js

---
+++ scripts/effects.js
@@ -0,0 +1,50 @@
+/*!
+ * Effects.js
+ * Made to protect against scammers
+ * by Deus Notam
+ * Telegram Channel → https://t.me/deusnotam
+ * Telegram Group → https://t.me/d3usn0tam
+ * Данный скрипт отвечает за эффекты.
+ */
+
+// Массив с эффектами
+const effects = {
+  //Ничего
+  none: () => {
+  },
+  //redirect - редирект на redirecturl
+  redirect: (redirectUrl) => {
+    console.log(`Применяется эффект redirect на ${redirectUrl}`);
+    window.location.replace(redirectUrl);
+  },
+  //thanos - поверх сайта чёрный экран с gif щелком таноса
+  thanos: (startDate) => {
+    console.log("Применяется эффект Thanos");
+    const blackScreen = document.createElement("div");
+  blackScreen.style.position = "fixed";
+  blackScreen.style.top = "0";
+  blackScreen.style.left = "0";
+  blackScreen.style.width = "100%";
+  blackScreen.style.height = "100%";
+  blackScreen.style.background = "black";
+  blackScreen.style.zIndex = "9999";
+  blackScreen.style.display = "flex";
+  blackScreen.style.flexDirection = "column";
+  blackScreen.style.alignItems = "center";
+  blackScreen.style.justifyContent = "center";
+
+  const gifImage = document.createElement("img");
+  gifImage.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWZ0bWRoajlyem1lbHViczd0MzU4YnlneHQ4cGRwMmhweTFsdzZjYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LOoaJ2lbqmduxOaZpS/giphy.gif";
+  gifImage.alt = "Tanos Snap";
+  gifImage.style.marginBottom = "20px";
+
+  const caption = document.createElement("p");
+  caption.style.color = "white";
+  caption.innerText = "Упс... Произошёл щелчок Таноса - сайт исчез...";
+
+  blackScreen.appendChild(gifImage);
+  blackScreen.appendChild(caption);
+
+  document.body.appendChild(blackScreen);
+  }
+};
