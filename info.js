b3815ca73adbeff40e7af16847612e53bfdf10c2
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-22 22:17:19 +0100

    Update info.js

--- info.js
+++ info.js
@@ -10,6 +10,12 @@ const d3usn0tamBlockerImg = document.createElement('img');
 d3usnotamBlockerA.href = 'https://deusnotam.github.io';
 d3usnotamBlockerA.textContent = 'D3usN0tam';
 d3usnotamBlockerA.classList.add('d3usn0taminfo');
+d3usn0tamBlockerSpan.style.zIndex = '2';
+d3usn0tamBlockerSpan.style.position = 'absolute';
+d3usn0tamBlockerSpan.style.transform = 'translateX(-50%)';
+d3usn0tamBlockerSpan.style.textAlign = 'center';
+d3usn0tamBlockerSpan.style.fontFamily = 'Comfortaa, cursive';
+d3usn0tamBlockerSpan.style.top = '100px';
 
 // Общее тело
 d3usn0tamBlockerDiv1.style.position = 'absolute';
@@ -52,7 +58,7 @@ d3usn0tamBlockerSpan.innerHTML = 'Данный сайт заблокирован
 
 // Обертка для текста с подложкой на анимацию
 d3usn0tamBlockerDiv2.style.position = 'absolute';
-d3usn0tamBlockerDiv2.style.top = '-100px';
+d3usn0tamBlockerDiv2.style.top = '-120px';
 d3usn0tamBlockerDiv2.style.transition = 'top 0.3s ease';
 
 d3usn0tamBlockerDiv1.addEventListener('mouseover', () => {
@@ -60,7 +66,7 @@ d3usn0tamBlockerDiv1.addEventListener('mouseover', () => {
 });
 
 d3usn0tamBlockerDiv1.addEventListener('mouseout', () => {
-    d3usn0tamBlockerDiv2.style.top = '-100px';
+    d3usn0tamBlockerDiv2.style.top = '-120px';
 });
 
 document.head.insertAdjacentHTML('beforeend', `
