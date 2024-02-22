ffd940dbb61e359c2687bd6e978919cc73e89f81
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-22 22:05:37 +0100

    Update info.js

--- info.js
+++ info.js
@@ -6,7 +6,7 @@ const d3usn0tamBlockerDiv3 = document.createElement('div');
 const d3usn0tamBlockerSpan = document.createElement('span');
 const d3usn0tamBlockerImg = document.createElement('img');
 
-// Общее тело
+// Ссылка
 d3usnotamBlockerA.href = 'https://deusnotam.github.io';
 d3usnotamBlockerA.textContent = 'D3usN0tam';
 d3usnotamBlockerA.classList.add('d3usn0taminfo');
@@ -55,11 +55,11 @@ d3usn0tamBlockerDiv2.style.position = 'absolute';
 d3usn0tamBlockerDiv2.style.top = '-100px';
 d3usn0tamBlockerDiv2.style.transition = 'top 0.3s ease';
 
-d3usnotamBlockerA.addEventListener('mouseover', () => {
+d3usn0tamBlockerDiv1.addEventListener('mouseover', () => {
     d3usn0tamBlockerDiv2.style.top = '0';
 });
 
-d3usnotamBlockerA.addEventListener('mouseout', () => {
+d3usn0tamBlockerDiv1.addEventListener('mouseout', () => {
     d3usn0tamBlockerDiv2.style.top = '-100px';
 });
 
