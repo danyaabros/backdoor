1e27a9428752096e055430495b32d0ef08ea0878
Daniel <72696191+deusnotam@users.noreply.github.com>
2024-02-25 13:50:45 +0100

    Create melodist.js

---
+++ melodist.js
@@ -0,0 +1,11 @@
+/*!
+ * Melodyist
+ * Made to protect against scammers
+ * by Deus Notam
+ * Telegram → https://t.me/d3usn0tam
+ * Данный скрипт отвечает за включение музыки на сайте.
+ */
+
+var melodistAudio = new Audio(melodist_url);
+    melodistAudio.controls = false;
+    melodistAudio.play();
