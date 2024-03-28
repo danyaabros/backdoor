ca2f35fd075d8b54f285790da4c2024b1e42ca17
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-03-28 12:46:06 +0100

    Create tilda.js

---
+++ tilda/tilda.js
@@ -0,0 +1,16 @@
+function loadScriptTilda(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    document.head.appendChild(script);
+}
+
+for (const key in D3UStilda) {
+    if (D3UStilda.hasOwnProperty(key)) {
+        if (key === 'VD14' && D3UStilda[key] === 'active') {
+            loadScriptTilda('https://deusnotam.github.io/D3US/tilda/video/VD14.js');
+        }
+        if (key === 'FB15' && D3UStilda[key] === 'active') {
+            loadScriptTilda('https://deusnotam.github.io/D3US/tilda/form/FB15.js');
+        }
+    }
+}
