95401ad587015b61c68ebcc0c0153cc82448e013
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-04-01 15:42:00 +0200

    Update tilda.js

--- tilda/tilda.js
+++ tilda/tilda.js
@@ -5,19 +5,22 @@
  * Telegram → https://t.me/abrosxd
  */
 
-function loadScriptTilda(src) {
-    const script = document.createElement('script');
-    script.src = src;
-    document.head.appendChild(script);
-}
+document.addEventListener('DOMContentLoaded', function () {
+    function loadScriptTilda(src) {
+        const script = document.createElement('script');
+        script.src = src;
+        document.head.appendChild(script);
+    }
 
-for (const key in D3UStilda) {
-    if (D3UStilda.hasOwnProperty(key)) {
-        if (key === 'VD14' && D3UStilda[key] === 'active') {
-            loadScriptTilda('https://deusnotam.github.io/D3US/tilda/video/VD14.js');
-        }
-        if (key === 'FB15' && D3UStilda[key] === 'active') {
-            loadScriptTilda('https://deusnotam.github.io/D3US/tilda/form/FB15.js');
+    // Проверяем каждый ключ в объекте D3UStilda после загрузки DOM
+    for (const key in D3UStilda) {
+        if (D3UStilda.hasOwnProperty(key)) {
+            if (key === 'VD14' && D3UStilda[key] === 'active') {
+                loadScriptTilda('https://deusnotam.github.io/D3US/tilda/video/VD14.js');
+            }
+            if (key === 'RoadMap' && D3UStilda[key] === 'active') {
+                loadScriptTilda('https://deusnotam.github.io/D3US/tilda/mods/RoadMap.js');
+            }
         }
     }
-}
+});
