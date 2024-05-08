740c3f367822e7eb8977bd994c86130b1f02fa85
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-05-08 08:50:00 +0200

    Update d3us.js

--- d3us.js
+++ d3us.js
@@ -4,8 +4,8 @@
  */
 
 if (!window.D3US) {
-  loadScript('https://deusnotam.github.io/D3US/system/activator.js');
-
+  const domain = 'https://deusnotam.github.io';
+  loadScript(`${domain}/D3US/system/activator.js`);
   fetch('https://app.nocodb.com/api/v1/db/data/noco/p2kmbphsgvqs8kz/mpqof3e6f1ueozo/views/vwz1zne8sfxvhxco', {
     method: 'GET',
     headers: {
@@ -15,7 +15,6 @@ if (!window.D3US) {
   .then(response => response.json())
   .then(data => handleData(data))
   .catch(error => console.error('Ошибка при получении данных:', error));
-
   function handleData(data) {
     if (data && data.list && Array.isArray(data.list)) {
       const D3USsite = data.list.map(item => ({
@@ -23,8 +22,7 @@ if (!window.D3US) {
         url: item.SiteURL,
         system: item.System,
         noti: item.Noti,
-        noti_title: item.NotiTitle,
-        noti_text: item.NotiText,
+        noti_array: item.NotiArray,
         blocker: item.Blocker,
         blocker_effect: item.BlockerEffect,
         blocker_redirecturl: item.BlockerRedirectURL,
@@ -39,7 +37,6 @@ if (!window.D3US) {
       console.error('Ошибка: Полученные данные не соответствуют ожидаемой структуре');
     }
   }
-
   function checkDomain(D3USsite) {
     const sites = D3USsite;
     const currentDomain = window.location.hostname;
@@ -51,23 +48,20 @@ if (!window.D3US) {
         "www." + currentDomain === siteHostname
       );
     });
-
     if (DeusSiteInfo && DeusSiteInfo.system === "on") {
       console.log("%cD3US System%c - %cConnected!%c\n\nProject Site → https://deusnotam.github.io", "font-weight: bold;", "", "color: #25ba1a; font-weight: bold;", "");
-      if (DeusSiteInfo.blocker === "active") loadScript('https://deusnotam.github.io/D3US/system/blocker.js');
-      if (DeusSiteInfo.noti === "active") loadScript('https://deusnotam.github.io/D3US/system/noti.js');
-      if (DeusSiteInfo.ads === "active") loadScript('https://deusnotam.github.io/D3US/system/ads.js');
-      if (DeusSiteInfo.tilda === "active") loadScript('https://deusnotam.github.io/D3US/tilda/tilda.js');
+      if (DeusSiteInfo.blocker === "on") loadScript(`${domain}/D3US/system/blocker.js`);
+      if (DeusSiteInfo.noti === "on") loadScript(`${domain}/D3US/system/noti.js`);
+      if (DeusSiteInfo.ads === "on") loadScript(`${domain}/D3US/system/ads.js`);
+      if (DeusSiteInfo.tilda === "on") loadScript(`${domain}/D3US/tilda/tilda.js`);
     } else if (!DeusSiteInfo) {
       console.log("%cD3US System%c\n\nСайт не найден в списке системы D3US.\nThe site was not found in the D3US system list.", "font-weight: bold;", "");
     } 
   }
-
   function loadScript(src) {
     const script = document.createElement('script');
     script.src = src;
     document.head.appendChild(script);
   }
-
   window.D3US = true;
 }
