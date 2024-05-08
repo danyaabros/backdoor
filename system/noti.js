1ae281bebe7503d7ca6efba9eb7a131256b41521
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-05-08 11:42:02 +0200

    Update noti.js

--- system/noti.js
+++ system/noti.js
@@ -6,8 +6,8 @@
 const notis = new abrosnoti(document.querySelector(".abrosnoti"));
 const message = DeusSiteInfo.noti_array;
 const array = eval(`[${message}]`);
-let i = array.length;
 setInterval(()=>{
+  let i = array.length;
   if (i == array.length) {
     notis.create("Deus System", "Кликни на уведомление, чтобы узнать, что оно тут делает", 0, true, () => {i = 0})
   } else if (i < array.length) {
