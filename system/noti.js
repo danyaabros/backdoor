ca67ed92f188a591ca0f0b24e3674c846c0c26b7
D3usN0tam <72696191+deusnotam@users.noreply.github.com>
2024-05-08 09:25:46 +0200

    Update noti.js

--- system/noti.js
+++ system/noti.js
@@ -5,13 +5,13 @@
 
 const notis = new abrosnoti(document.querySelector(".abrosnoti"))
 const message = [DeusSiteInfo.noti_array];
-let i = 1;
+let number = 1;
 message[0]()
 setInterval(()=>{
-  if (i == message.length) {
+  if (number == message.length) {
     notis.create("Demo done", "click on this notification to restart the demo or go look at the code if you're interested", 0, true, () => {i = 0})
-  } else if (i < message.length) {
-    message[i]()
+  } else if (number < message.length) {
+    message[number]()
   }
-  i++
+  number++
 }, 4000)
